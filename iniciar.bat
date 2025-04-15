dedc8dca0d543466070c6c10b325263d2ee0a70d
Evandro Almeida <136964896+EvandroAlmeidaDev@users.noreply.github.com>
2025-04-15 09:30:51 -0300

    Delete iniciar.bat

--- iniciar.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Iniciando servidor...
-echo.
-echo Acesse http://localhost:8080 no seu navegador
-echo.
-echo Para parar o servidor, pressione Ctrl+C e depois S para confirmar
-echo.
-node server.js 
