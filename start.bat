58cf7051daea41bac3d2fd26f8e8a76553769f05
Evandro Almeida <136964896+EvandroAlmeidaDev@users.noreply.github.com>
2025-04-15 09:29:56 -0300

    Delete start.bat

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Iniciando servidor Node.js...
-echo.
-echo Quando o servidor estiver rodando, acesse:
-echo http://localhost:3000/
-echo.
-echo Pressione Ctrl+C para parar o servidor.
-echo.
-node server.js 
